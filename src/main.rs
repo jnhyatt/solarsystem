63535f221af19e4ffa341b2dd1b304b56139a724
Josh Hyatt <joshua.n.hyatt@gmail.com>
2025-02-11 18:52:39 -0700

    Mesh subdivision

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use bevy::{
     math::{dvec3, DVec3},
     pbr::wireframe::{Wireframe, WireframePlugin},
     prelude::*,
-    render::mesh::PrimitiveTopology,
+    render::mesh::{Indices, PrimitiveTopology},
     window::CursorGrabMode,
 };
 use bevy_enhanced_input::prelude::*;
 use big_space::{
-    commands::BigSpaceCommands,
-    floating_origins::FloatingOrigin,
-    grid::{cell::GridCell, local_origin::Grids, Grid},
+    commands::BigSpaceCommands, floating_origins::FloatingOrigin, grid::Grid,
     plugin::BigSpacePlugin,
 };
+
+type P = i64;
 
 #[derive(Clone, Copy)]
 struct FaceCoord {
@@ -50,8 +50,8 @@ impl IcoFace {
 /// This represents any sub-face of arbitrary order of an icosphere. A 0th-order sub-face of e.g.
 /// face 0 is face 0. Each face has 4 1st order sub-faces, 16 2nd order sub-faces, and so on.
 struct SubFace {
-    face: IcoFace,
     coords: [FaceCoord; 3],
+    radius: f64,
 }
 
 impl SubFace {
@@ -61,39 +61,78 @@ impl SubFace {
         let ca = self.coords[2].midpoint(self.coords[0]);
         [
             Self {
-                face: self.face,
                 coords: [self.coords[0], ab, ca],
+                ..self
             },
             Self {
-                face: self.face,
                 coords: [self.coords[1], bc, ab],
+                ..self
             },
             Self {
-                face: self.face,
                 coords: [self.coords[2], ca, bc],
+                ..self
             },
             Self {
-                face: self.face,
                 coords: [ab, bc, ca],
+                ..self
             },
         ]
     }
 
     fn mesh(self) -> BigMesh {
-        let offset = self.coords.into_iter().map(|x| x.position).sum::<DVec3>() / 3.0;
+        let vertices = self
+            .coords
+            .into_iter()
+            .map(|x| x.position)
+            .collect::<Vec<_>>();
+        let faces = [[0, 1, 2]];
+        let vertices = std::cell::Cell::new(vertices);
+        let subdivide = |[a, b, c]: [usize; 3]| {
+            let mut my_vertices = vertices.take();
+            my_vertices.push(my_vertices[a].midpoint(my_vertices[b]).normalize());
+            let ab = my_vertices.len() - 1;
+            my_vertices.push(my_vertices[b].midpoint(my_vertices[c]).normalize());
+            let bc = my_vertices.len() - 1;
+            my_vertices.push(my_vertices[c].midpoint(my_vertices[a]).normalize());
+            let ca = my_vertices.len() - 1;
+            vertices.set(my_vertices);
+            [[a, ab, ca], [b, bc, ab], [c, ca, bc], [ab, bc, ca]]
+        };
+        let faces = faces
+            .into_iter()
+            .flat_map(subdivide)
+            .flat_map(subdivide)
+            .flat_map(|x| x.map(|x| x as u32)) // Flatten [[usize; 3]] -> [u32]
+            .collect::<Vec<_>>();
+        let vertices = vertices.into_inner();
+        for x in &vertices {
+            assert!((x.length() - 1.0).abs() < 1e-13); // a micrometer at planet scale
+        }
+        let offset = vertices.iter().sum::<DVec3>() * self.radius / (vertices.len() as f64);
         let mesh = Mesh::new(
             PrimitiveTopology::TriangleList,
             RenderAssetUsages::default(),
         )
         .with_inserted_attribute(
             Mesh::ATTRIBUTE_POSITION,
-            self.coords
-                .into_iter()
-                .map(|x| (x.position - offset).as_vec3())
+            vertices
+                .iter()
+                .inspect(|x| {
+                    let expected = self.radius * *x - offset;
+                    let converted = expected.as_vec3();
+                    let actual = converted.as_dvec3();
+                    let difference = expected.distance(actual);
+                    println!(
+                        "Difference of {difference} at magnitude of {}",
+                        expected.length().log10()
+                    );
+                })
+                .map(|x| (self.radius * x - offset).as_vec3())
                 .collect::<Vec<_>>(),
         )
-        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![Vec2::ZERO; 3])
-        .with_computed_flat_normals();
+        .with_inserted_indices(Indices::U32(faces))
+        .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![Vec2::ZERO; vertices.len()])
+        .with_computed_normals();
         BigMesh { mesh, offset }
     }
 }
@@ -110,7 +149,7 @@ fn main() {
             DefaultPlugins.build().disable::<TransformPlugin>(),
             WireframePlugin,
             EnhancedInputPlugin,
-            BigSpacePlugin::<i32>::default(),
+            BigSpacePlugin::<P>::default(),
         ))
         .add_input_context::<SpaceshipControls>()
         .add_observer(translation_input)
@@ -137,15 +176,11 @@ struct SpeedText;
 
 fn translation_input(
     input: Trigger<Fired<Translation>>,
-    mut player: Query<(&mut Transform, &mut GridCell<i32>, &Speed)>,
-    grids: Grids<i32>,
+    mut player: Query<(&mut Transform, &Speed)>,
     time: Res<Time>,
 ) {
-    let (mut transform, mut grid_cell, &Speed(speed)) = player.get_mut(input.entity()).unwrap();
-    let movement = transform.transform_point(input.value * speed * time.delta_secs());
-    let grid = grids.parent_grid(input.entity()).unwrap();
-    let (cell_offset, movement) = grid.translation_to_grid(movement);
-    *grid_cell += cell_offset;
+    let (mut transform, &Speed(speed)) = player.get_mut(input.entity()).unwrap();
+    let movement = transform.rotation * (input.value * speed * time.delta_secs());
     transform.translation += movement;
 }
 
@@ -186,13 +221,13 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
             parent.spawn((SpeedText, Text::new("Speed: 0m/s")));
             parent.spawn(Text::new("Press `Tab` to toggle cursor capture"));
         });
-    commands.spawn_big_space(Grid::<i32>::new(1.0, 0.0), |grid| {
+    commands.spawn_big_space(Grid::<P>::new(1e-3, 0.0), |grid| {
         grid.spawn_spatial((
             SpaceshipControls,
             Speed(1.0),
             Camera3d::default(),
             Projection::Perspective(PerspectiveProjection {
-                near: 1e-6,
+                near: 1e-8,
                 ..default()
             }),
             Transform::from_xyz(0.0, 0.0, 10.0),
@@ -215,7 +250,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
             dvec3(PHI, 0.0, 1.0),
         ]
         .map(|x| x.normalize());
-        let faces = [
+        let mut faces = [
             IcoFace::new(0, 2, 10),
             IcoFace::new(0, 10, 6),
             IcoFace::new(0, 6, 4),
@@ -237,8 +272,8 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
             IcoFace::new(10, 11, 6),
             IcoFace::new(10, 7, 11),
         ]
+        .into_iter()
         .map(|face| SubFace {
-            face,
             coords: [
                 FaceCoord {
                     position: vertices[face.vertices[0]],
@@ -253,9 +288,15 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
                     barycentric_coords: [0.0, 0.0, 1.0],
                 },
             ],
+            radius: 6.4e6, // Earth
         })
-        .map(|sub_face| sub_face.mesh());
+        .collect::<Vec<_>>();
+        for _ in 0..48 {
+            let new_faces = faces.pop().unwrap().subdivide();
+            faces.extend(new_faces);
+        }
         for mesh in faces {
+            let mesh = mesh.mesh();
             let (cell, offset) = grid.grid().translation_to_grid(mesh.offset);
             let mesh = meshes.add(mesh.mesh);
             grid.spawn_spatial((
@@ -298,15 +339,17 @@ impl InputContext for SpaceshipControls {
             KeyCode::KeyS.with_modifiers(SwizzleAxis::YZX),
             KeyCode::KeyW.with_modifiers((SwizzleAxis::YZX, Negate::all())),
         ));
-        ctx.bind::<Rotation>().to((
-            Input::mouse_motion().with_modifiers((
-                SwizzleAxis::YXZ,
-                Negate::all(),
-                Scale::splat(0.1),
-            )),
-            KeyCode::KeyQ.with_modifiers(SwizzleAxis::YZX),
-            KeyCode::KeyE.with_modifiers((SwizzleAxis::YZX, Negate::all())),
-        ));
+        ctx.bind::<Rotation>()
+            .to((
+                Input::mouse_motion().with_modifiers((
+                    SwizzleAxis::YXZ,
+                    Negate::all(),
+                    Scale::splat(0.1),
+                )),
+                KeyCode::KeyQ.with_modifiers(SwizzleAxis::YZX),
+                KeyCode::KeyE.with_modifiers((SwizzleAxis::YZX, Negate::all())),
+            ))
+            .with_modifiers(Negate::x()); // x here is rotation about x, not mouse x motion
         ctx.bind::<AdjustSpeed>()
             .to(Input::mouse_wheel().with_modifiers((SwizzleAxis::YXZ, Scale::splat(2.4))));
         ctx.bind::<ToggleCursorCapture>().to(KeyCode::Tab);
